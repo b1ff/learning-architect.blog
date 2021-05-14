3c07728a384dbd63a7ae403d8435864ee4b6e544
Evgeniy Moroz <evgeniy.moroz@gmail.com>
2021-05-14 23:11:36 +0300

    Add first article and simple styling.

---
+++ src/components/RecentPublications.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './RecentPublications.scss';
+
+export function RecentPublications() {
+	return <nav className={'recent-publications'}>
+		<h3>Последние публикации</h3>
+		<ul>
+			<li>хх.05.2021 - <a href="/architect-team-discussions-part-1/">Дискуссии в архитектурной команде</a></li>
+			<li><a href="/">К списку тем</a></li>
+		</ul>
+	</nav>;
+}
