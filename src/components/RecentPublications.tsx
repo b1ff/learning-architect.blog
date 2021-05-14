65867f34093ab146f2872ca1067c0777159271f3
Evgeniy Moroz <evgeniy.moroz@gmail.com>
2021-05-15 00:02:49 +0300

    Fix links on github-pages and remove style for list on index page.

--- src/components/RecentPublications.tsx
+++ src/components/RecentPublications.tsx
@@ -5,8 +5,8 @@ export function RecentPublications() {
 	return <nav className={'recent-publications'}>
 		<h3>Последние публикации</h3>
 		<ul>
-			<li>хх.05.2021 - <a href="/architect-team-discussions-part-1/">Дискуссии в архитектурной команде</a></li>
-			<li><a href="/">К списку тем</a></li>
+			<li>хх.05.2021 - <a href="./architect-team-discussions-part-1/">Дискуссии в архитектурной команде</a></li>
+			<li><a href="../">К списку тем</a></li>
 		</ul>
 	</nav>;
 }
