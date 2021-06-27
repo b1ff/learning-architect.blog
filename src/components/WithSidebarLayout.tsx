15902c1d9736d356907487ed9bc3ce4f58803587
Evgeniy Moroz <evgeniy.moroz@namecheap.com>
2021-06-27 12:20:43 +0300

    Added sticky position to about section.

--- src/components/WithSidebarLayout.tsx
+++ src/components/WithSidebarLayout.tsx
@@ -7,8 +7,10 @@ export function WithSidebarLayout({ children }) {
 		<div className='container bg-white max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-16'>
 			<div className='grid grid-cols-1 md:grid-cols-12 shadow-lg'>
 				<div className="col-span-3 p-6 border-r">
-					<About/>
-					<RecentPublications/>
+					<div className="sticky top-0">
+						<About/>
+						<RecentPublications/>
+					</div>
 				</div>
 				<main className="col-span-8 p-6">
 					{children}
