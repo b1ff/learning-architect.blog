8ed9849ffb33d4c3943195aa7599e2bd1783c6e9
Evgeniy Moroz <evgeniy.moroz@namecheap.com>
2021-06-27 12:11:18 +0300

    Added some text to about section and contacts to reach owner.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,5 +3,12 @@ import * as React from 'react';
 export function About() {
 	return (<section>
 		<h3 className="sub-h">Про блог</h3>
+		<p className="py-5">Записываю мысли о работе и не только. Попытка систематизировать информацию и записать, то что приходиться так или иначе повторять.</p>
+		<h3 className="sub-h">Про меня</h3>
+		<p className="pt-5">Привет, меня зовут Евгений.</p>
+		<p className="py-5">
+			Начиная с 2019 формально работаю архитектором. До сих пор разбираюсь что это значит и как с этим жить.
+			<br />Написать можно сюда <a className="link-default" href={"mailto:admin@learning-architect.blog"}>admin@learning-architect.blog</a>
+		</p>
 	</section>);
 }
--- src/components/WithSidebarLayout.tsx
+++ src/components/WithSidebarLayout.tsx
@@ -4,17 +4,15 @@ import { About } from './About';
 
 export function WithSidebarLayout({ children }) {
 	return (
-		<div>
-			<div className='container bg-white max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-16'>
-				<div className='grid grid-cols-1 md:grid-cols-12 shadow-lg'>
-					<div className="col-span-3 p-6">
-						<About/>
-						<RecentPublications/>
-					</div>
-					<main className="col-span-8 p-6">
-						{children}
-					</main>
+		<div className='container bg-white max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0 py-16'>
+			<div className='grid grid-cols-1 md:grid-cols-12 shadow-lg'>
+				<div className="col-span-3 p-6 border-r">
+					<About/>
+					<RecentPublications/>
 				</div>
+				<main className="col-span-8 p-6">
+					{children}
+				</main>
 			</div>
 		</div>
 	)
