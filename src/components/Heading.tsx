614aaeb0f4bcd7228c9b6a56d4c817a8428a82ac
Evgeniy Moroz <evgeniy.moroz@namecheap.com>
2023-03-28 17:49:06 +0100

    Started to remove occupants language from the blog. Translated tests article.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 
-export const title = 'Блог обучающегося архитектора';
-export const description = 'Всё около программной архитектуры чему обучаюсь во время работы и не только.';
-const defaultKeywords = 'architecture,архитектура,программное обеспечение,DevOps,девопс,software'
+export const title = 'From Novice to Architect: My Journey of Learning and Growth';
+export const description = 'Everything about software architecture that I am learning both at work and beyond.';
+const defaultKeywords = 'architecture,DevOps,software,personal experience'
 
 interface Props {
 	additionalKeywords?: string;
@@ -11,7 +11,7 @@ interface Props {
 
 export class Heading extends React.Component<Props> {
 	render() {
-		return <Helmet htmlAttributes={{ lang: 'ru' }}>
+		return <Helmet htmlAttributes={{ lang: 'en' }}>
 			<title>{title}</title>
 			<meta name="title" content={title}/>
 			<meta name="description" content={description}/>
